8abfc33741f55abf953b3a6452bae683f56d2fd9
Thoth Gunter <thothgunter@live.com>
2025-02-16 20:56:46 -0500

    added ghostty config

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ cp after/syntax/syncolor.vim ~/.vim/after/syntax/.
 cp after/syntax/toml.vim ~/.vim/after/syntax/.
 cp vim-lsp-settings/settings.json ~/.local/share/vim-lsp-settings/.
 
+# move the ghostty config where it needs to be.
+if [[ $OSTYPE == "linux-gnu" ]]; then
+    cp ghostty/config ~/.config/ghostty
+
+elif [[ $OSTYPE == "darwin" ]]; then
+    cp ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
+
+else
+    echo "Unknown os"
+fi
