0710b1ba1b29518e7a48df16fe5bd5df6f3e37e6
Thoth Gunter <thothgunter@live.com>
2026-03-04 08:57:04 -0500

    cleaned up setup script and removed un-used files

--- after/syntax/syncolor.vim
+++
@@ -1,2 +0,0 @@
-highlight Todo ctermbg=None ctermfg=lightyellow cterm=underline,bold
-highlight notesTodo ctermbg=None ctermfg=lightyellow cterm=underline,bold
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
+
 # script should copy files to where ever they belong.
-cp vimrc ~/.vimrc
-cp after/syntax/syncolor.vim ~/.vim/after/syntax/.
-cp after/syntax/toml.vim ~/.vim/after/syntax/.
 cp vim-lsp-settings/settings.json ~/.local/share/vim-lsp-settings/.
 cp init.vim ~/.config/nvim/.
 cp search_tags.sh ~/.local/bin/.
@@ -22,7 +21,7 @@ if [[ $OSTYPE == "linux-gnu" ]]; then
 # See below
 # `--> echo $OSTYPE
 # darwin24.0
-elif [[ "darwin" =~ $OSTYPE ]]; then
+elif [[ $OSTYPE =~ "darwin" ]]; then
     cp ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
     # TODO
     # Lets move this into some separate scripts
@@ -32,10 +31,10 @@ elif [[ "darwin" =~ $OSTYPE ]]; then
     # When attempting to move to the end of a line using `SHIFT-4` I often release the shift before releasing the 4.
     # The time between the release of these two keys is often long enough to trigger repeat.
     # Though in this case the `4` is repeated resulting in unwanted repeated inputs.
-    echo "Currently set key repeat delay"
-    defaults read NSGlobalDomain InitialKeyRepeat
-    echo "Setting the new key repeat delay to `50`"
+    delay=$(defaults read NSGlobalDomain InitialKeyRepeat)
+    echo "Currently key repeat delay settings: ${delay}"
+    echo "Setting the new key repeat delay to '50'"
     defaults write NSGlobalDomain InitialKeyRepeat -int  50
 else
-    echo "Unknown os"
+    echo "Unknown os: $OSTYPE"
 fi
